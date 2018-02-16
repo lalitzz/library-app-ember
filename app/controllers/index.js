0848eebac722cee5162199bb750ca2d7b264a729
lalit527 <lalit.slg007@gmail.com>
2018-02-16 14:14:30 +0530

    Before Lunch

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,15 +1,19 @@
-import Controller from '@ember/controller';
 import { computed, observer } from '@ember/object';
+import { empty, match, not } from '@ember/object/computed';
+import Controller from '@ember/controller';
 
 export default Controller.extend({
-	isDisabled: true,
+	
+	headerMessage: 'Coming Soon',
+	responseMessage: '',
 	emailAddress: '',
-
-    actualEmailAddress: computed('emailAddress', function(){
-    	console.log("Actual Email Address Funtion is called", this.get('emailAddress'));
-    }),
-
-    emailAddressChanged: observer('emailAddress', function(){
-    	console.log("observer is called", this.get('emailAddress'));
-    })
+    isValid: match('emailAddress', /^.+@.+\..+$/),
+    isDisabled: not('isValid'),
+    actions: {
+    	saveInvitation() {
+    		alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+    		this.set('responseMessage', `Thank You, we have save the following email address: ${this.get('emailAddress')}`);
+    		this.set('emailAddress', '');
+    	}
+    }
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+	actions:{
+		prevent(e){
+			e.preventDefault();
+			return false;
+		}
+	}
 });
