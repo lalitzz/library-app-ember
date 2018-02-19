836a9b66b5a696f96bb2446f9b73e35b5dfb876c
lalit527 <lalit.slg007@gmail.com>
2018-02-19 17:35:16 +0530

    Before making dummy data with faker

---
+++ app/components/fader-label.js
@@ -0,0 +1,26 @@
+import Component from '@ember/component';
+import { observer } from '@ember/object';
+import { later, cancel } from '@ember/runloop';
+
+export default Component.extend({
+	tagName: 'span',
+
+	className: ['label label-success label-fade'],
+	classNameBinding: ['isShowing: label-show'],
+
+	isShowing: false,
+
+	isShowingChanged: observer('isShowing', function(){
+		this._runLater = later(() => this.set('isShowing', false), 3000);
+	}),
+
+	resetRunLater() {
+		this.set('isShowing', false);
+		cancel(this._runLater);
+	},
+
+	willDestroy() {
+		this.resetRunLater();
+		this._super(...arguments);
+	}
+});
